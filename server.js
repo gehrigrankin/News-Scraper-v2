2e8d9fd4e6688b1769f2f03c58124507e9853cc6
Gehrig <rgrankin22@gmail.com>
2020-11-28 00:09:47 -0700

    added users model

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const connectDB = require('./config/db')
 const path = require('path');
 
 const app = express();
 
+// Connect database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API running'))
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
 
 const port = process.env.PORT || 8080;
 

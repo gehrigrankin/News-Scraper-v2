04ab9b49410394f77fb33351847290f4e73fd2c0
Gehrig <rgrankin22@gmail.com>
2020-11-30 13:54:49 -0700

    logout functionality

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { connect } from 'react-redux';
+import { logout } from '../../actions/auth';
+import PropTypes from 'prop-types';
 
 import "./Navbar.css";
 
-const Navbar = () => {
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+    const authLinks = (
+        <a onClick={logout} href="#!">
+            <span class="icon">
+                <i class="fas fa-home"></i>
+            </span>{' '}
+            Logout
+        </a>
+    )
+
+    // const guestLinks = (
+
+    // )
+
     return (
         <nav className="navbar is-success">
             <div className="navbar-brand">
                 <Link className="navbar-item" to="/">
                     AZ Central Scraper
                 </Link>
-            
+
                 <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
@@ -21,12 +37,22 @@ const Navbar = () => {
                 <div className="navbar-end">
                     <Link className="navbar-item" to="/saved">Saved Articles</Link>
                     <Link className="navbar-item" to="/">Home</Link>
+                    { !loading && isAuthenticated ? authLinks : null }
                 </div>
             </div>
-            
+
         </nav>
 
     )
 }
 
-export default Navbar
+Navbar.propTypes = {
+    logout: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    auth: state.auth
+})
+
+export default connect(mapStateToProps, { logout })(Navbar);

8952e372d704a34fb4ab685332a99ee0da30a054
Gehrig <rgrankin22@gmail.com>
2020-12-01 20:54:57 -0700

    prepared for deployment

--- server.js
+++ server.js
@@ -10,30 +10,30 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API running'))
-
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/article', require('./routes/api/article'));
 app.use('/api/azc', require('./routes/api/azc'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  
+  app.get('*', function (req, res) {
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 8080;
 
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+// // the __dirname is the current directory from where the script is running
+// app.use(express.static(__dirname));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
+// app.get('/ping', function (req, res) {
+//  return res.send('pong');
+// });
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-}
 
 app.listen(port, () => console.log(`server started on port ${port}`));
